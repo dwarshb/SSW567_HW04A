9f6282b9bcf81ca2a58bd196e7d6632ee78a5f9a
dwarshb <dwarsh.b@gmail.com>
2023-02-24 21:49:56 -0500

    Added outputs

--- testGitHub567.py
+++ testGitHub567.py
@@ -1,22 +1,21 @@
 import unittest
 import mock
 
-import GitHub567
-from GitHub567 import gitHubApi
+import GithubAPI
 
 
 # Mocking service calls
-@mock.patch('GitHub567.gitHubApi', return_value=False)
+@mock.patch('GithubAPI.gitHubApi', return_value=False)
 class testRepo(unittest.TestCase):
     # Test case for when user is not present
     def testNoUser(self, mock_check_output):
-        actual_result = GitHub567.gitHubApi()
+        actual_result = GithubAPI.gitHubApi()
         expected_result = False
         self.assertEqual(expected_result, actual_result)  # Checking the final and expected result
 
     # Test case for when user is found
     def testUserFound(self, mock_check_output):
-        actual_directory = GitHub567.gitHubApi()
+        actual_directory = GithubAPI.gitHubApi()
         expected_directory = True
         self.assertNotEqual(expected_directory, actual_directory)  # Checking the final and expected result
 
